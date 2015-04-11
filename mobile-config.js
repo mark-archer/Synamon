217168413a4b96084e5c1a86e289b6884398d50f
Mark Archer <marcher@uci.edu>
2015-04-11 11:48:08 -0700

    ignoring files

--- mobile-config.js
+++ mobile-config.js
@@ -1,9 +1,9 @@
 App.info({
-  name: 'Todos',
-  description: 'A simple todo list app built in Meteor.',
-  author: 'Percolate Studio Team',
-  email: 'us@percolatestudio.com',
-  website: 'http://percolatestudio.com',
+  name: 'NewOS',
+  description: 'An application to help people with Autism learn synonyms.',
+  author: 'NewOS',
+  email: 'marcher@uci.edu',
+  website: 'http://newos.meteor.com/',
   version: '0.0.1'
 });
 
