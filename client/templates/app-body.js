86764d53676acee97da0a1aec6dd9fe8dec149ed
Mark Archer <marcher@uci.edu>
2015-04-11 11:29:37 -0700

    first

---
+++ client/templates/app-body.js
@@ -0,0 +1,76 @@
+var MENU_KEY = 'menuOpen';
+Session.setDefault(MENU_KEY, false);
+
+var USER_MENU_KEY = 'userMenuOpen';
+Session.setDefault(USER_MENU_KEY, false);
+
+var SHOW_CONNECTION_ISSUE_KEY = 'showConnectionIssue';
+Session.setDefault(SHOW_CONNECTION_ISSUE_KEY, false);
+
+var CONNECTION_ISSUE_TIMEOUT = 5000;
+
+Meteor.startup(function () {
+  // set up a swipe left / right handler
+  $(document.body).touchwipe({
+    wipeLeft: function () {
+      Session.set(MENU_KEY, false);
+    },
+    wipeRight: function () {
+      Session.set(MENU_KEY, true);
+    },
+    preventDefaultEvents: false
+  });
+
+  // Only show the connection error box if it has been 5 seconds since
+  // the app started
+  setTimeout(function () {
+    // Launch screen handle created in lib/router.js
+    dataReadyHold.release();
+
+    // Show the connection error box
+    Session.set(SHOW_CONNECTION_ISSUE_KEY, true);
+  }, CONNECTION_ISSUE_TIMEOUT);
+});
+
+Template.appBody.onRendered(function() {
+  this.find('#content-container')._uihooks = {
+    insertElement: function(node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn(function () {
+          listFadeInHold.release();
+        });
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
+    }
+  };
+});
+
+Template.appBody.helpers({
+  // We use #each on an array of one item so that the "list" template is
+  // removed and a new copy is added when changing lists, which is
+  // important for animation purposes. #each looks at the _id property of it's
+  // items to know when to insert a new item and when to update an old one.
+  cordova: function() {
+    return Meteor.isCordova && 'cordova';
+  },
+  emailLocalPart: function() {
+    var email = Meteor.user().emails[0].address;
+    return email.substring(0, email.indexOf('@'));
+  },
+  connected: function() {
+    if (Session.get(SHOW_CONNECTION_ISSUE_KEY)) {
+      return Meteor.status().connected;
+    } else {
+      return true;
+    }
+  }
+});
+
+Template.appBody.events({
+
+});
